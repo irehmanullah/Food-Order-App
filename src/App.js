2f7a7a199d0128571bf1105763ebc11cadc41384
irehmanullah <irehmanullah@gmail.com>
2022-01-15 21:25:43 +0500

    last commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Header from './Components/Layout/Header';
 import Meals from './Components/Meals/Meals';
 import Cart from './Components/Cart/Cart'
-import CartProvider from './store/CardProvider';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
